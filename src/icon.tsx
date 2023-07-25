3ee045c2299200a3b10d8e4c9fe8797044af9517
taytay <1273724496@qq.com>
2023-07-25 11:35:47 +0800

    更改onClick类型

--- src/icon.tsx
+++ src/icon.tsx
@@ -4,11 +4,12 @@ import './icon.scss';
 
 interface IconProps{
     name:string
+    onClick:React.MouseEventHandler<SVGElement>
 }
 const Icon:React.FunctionComponent<IconProps> = (props) => {
     return (
 
-            <svg className='yu-icon'>
+            <svg className='yu-icon' onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`}></use>
             </svg>
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Icon from './icon'
 
-ReactDOM.render(<div><Icon name='fish' /><Icon name='qq' /></div>,document.body.querySelector('#root'))
+const fn:React.MouseEventHandler = (e) =>{
+    console.log(e.target)
+}
+ReactDOM.render(<div><Icon onClick={fn} name='qq' /></div>,document.body.querySelector('#root'))
