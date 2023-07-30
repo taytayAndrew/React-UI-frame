bf18bd5a1964783be1f15b109f6d1d4d145d225f
taytay <1273724496@qq.com>
2023-07-30 15:34:20 +0800

    完成dialog的三个便捷API

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import './dialog.scss'
 import Icon from "../icon";
+import '../index.scss'
+import ReactDOM from "react-dom";
 
 interface Props {
     visible:boolean
+    button?:Array<ReactElement>
+    onClose:React.MouseEventHandler
+    onClickCloseMask?:boolean
 }
 function scpoeClass(prefix:string) {
    return function addYu(name?:string) {
@@ -13,36 +18,92 @@ function scpoeClass(prefix:string) {
  
  const Dialog:React.FunctionComponent<Props>=(props)=> {
     const sc = scpoeClass('yu-dialog')
-    return(
-        props.visible 
-        ?
-        <>
-       
-        <div className={sc('mask')}></div>
-        <div className={sc()}>
-        <div className={sc('close')}>
-                <Icon  name='close' />
-            </div>
-            <header className={sc('header')}>
-                提示
-            </header>
-            <main className={sc('main')}>
-            {props.children}
-            </main>
-            <footer className={sc('footer')}>
-               <button>确认</button>
-               <button>取消</button>
-            </footer>
+    const onClickClose:React.MouseEventHandler = (e) => {
+        props.onClose(e)
+    }
+    const onClickMask:React.MouseEventHandler = (e) => {
+        if(props.onClickCloseMask){
+        props.onClose(e)
 
+        }
+    }
+    const x = props.visible 
+    ?
+    <>
+   
+    <div className={sc('mask')} onClick={onClickMask}></div>
+    <div className={sc()}>
+    <div className={sc('close')} onClick={onClickClose}>
+            <Icon  name='close' />
         </div>
-           
-  
-
-        </>
-       
-        :
-        null
+        <header className={sc('header')}>
+            提示
+        </header>
+        <main className={sc('main')}>
+        {props.children}
+        </main>
+        {
+            props.button && props.button.length>0 &&
+            <footer className={sc('footer')}>
+           {props.button&&props.button.map((button,index) => React.cloneElement(
+            button,{key:index}
+           ))}
+        </footer>
+        }
+    </div>
+    </>
+    :
+    null
+    return(
+        ReactDOM.createPortal(x,document.body)
     )
 }
+Dialog.defaultProps = {
+    onClickCloseMask:false
+}
+export const modal = (content: ReactNode,button?:Array<ReactElement>,afterClose?:() => void) => {
+    const close =()=> {
+        ReactDOM.render(React.cloneElement(component,{visible:false}),div)
+        ReactDOM.unmountComponentAtNode(div) //将div从组件上卸载下来
+        div.remove //将div删除
+    }
+    const component = <Dialog 
+    visible= {true} 
+    onClose ={() =>{
+        close();
+        afterClose&&afterClose()
+    }}
+    button={button}
+    >
+    {content}
+    </Dialog>
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component,div)
+  return close;
+    
+}
+export const alert = (content: string) => {
+    const button = <button onClick={() => close()}>OK</button>
+    const close = modal(content,[button])
+
+}
+export const confirm = (content:string,yes?:() => void,no?:() => void) => {
+    const onYes = () => {
+       close()
+       yes&&yes()
+    }
+    const onNo =() =>{
+        close()
+        no&&no()
+
+    }
+    const button=[
+        <button style={{backgroundColor: '#0069d9'}} onClick={onYes}>yes</button>,
+        <button style={{backgroundColor: '#c82333'}} onClick={onNo}>no</button>
+    ]
+    const close = modal(content,[...button])
+}
+
 
 export default Dialog
--- src/dialog/dialogExample.tsx
+++ src/dialog/dialogExample.tsx
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
-import Dialog from "./dialog";
+import Dialog,{alert , confirm,modal} from "./dialog";
 
 export default function (){
     const [x,setX] = useState(false)
+    const [y,setY] = useState(false)
+    const openModal = () => {
+        const close = modal(
+            <h1>你好
+                <button onClick={() => close()}>close</button>
+            </h1>
+        )
+    }
+    //由于函数延迟执行 onClick中执行的是close函数 也就是onClick等于modal的返回值
     return(
         <>
+        <div style={{position:'relative',zIndex : '10'}}>
+        <h1>example 1</h1>
         <button onClick={() => {setX(!x)}} >button</button>
-        <Dialog  visible={x}>
+        <Dialog  visible={x} button={[
+            <button style={{backgroundColor: '#0069d9'}} onClick ={() => {setX(false)}}>1</button>,
+            <button style={{backgroundColor: '#c82333'}} onClick ={() => {setX(false)}}>1</button>
+        ]} onClose = {() => setX(false)}>
+            <>
+            <strong>hi</strong>
+            </>
+        </Dialog>
+        </div>
+        <div>
+        <h1>example 2</h1>
+        <button onClick={() => {setY(!y)}} >button</button>
+        <Dialog  visible={y} button={[
+            <button style={{backgroundColor: '#0069d9'}} onClick ={() => {setY(false)}}>1</button>,
+            <button style={{backgroundColor: '#c82333'}} onClick ={() => {setY(false)}}>1</button>
+        ]} onClose = {() => setY(false)} onClickCloseMask ={true}>
             <>
             <div>hi</div>
             </>
         </Dialog>
+        </div>
+        <div>
+            <h1>example 3</h1>
+            <button onClick={() => alert('alert')}>alert</button>
+            <button onClick={() => confirm('1',()=>{console.log('1')},()=>{console.log('2')})} >conform</button>
+        </div>
+        <div>
+            <h1>example 4</h1>
+            <button onClick={openModal}>modal</button>
+           
+        </div>
         </>
     )
 }
